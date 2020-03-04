c300cde809a5cc86e5824ef8bba4396edfba9bbb
yexhoo (Josue Nolasco) <josue.nolasco@gmail.com>
2020-03-03 19:21:31 -0600

    Dockerfile and docker-compose file was included for database connection

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import { loadControllers, scopePerRequest } from "awilix-express";
 import express, { Application } from "express";
 import setup from "./setup";
+import dotenv from "dotenv"
 
 const app: Application = express();
-const PORT = 3000
+dotenv.config();
 
 app.use(express.json());
 app.use(scopePerRequest(setup.getContainer()));
 app.use(loadControllers("controllers/**/*.ts", { cwd: __dirname }));
 app.use(loadControllers("controllers/**/*.js", { cwd: __dirname }));
-app.listen(PORT, () => console.info("Transactional PIN service is running ..."));
+app.listen(process.env.PORT, () => console.info("Transactional PIN service is running ..."));
