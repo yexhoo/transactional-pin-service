d995e821484c8b951149064ce01f942b1811625a
yexhoo (Josue Nolasco) <josue.nolasco@gmail.com>
2020-03-04 21:38:25 -0600

    rollback update password

--- src/services/credentialService.ts
+++ src/services/credentialService.ts
@@ -4,8 +4,6 @@ import Credential from "../model/credential"
 import KeyDerivation from "./ciphers/keyDerivation"
 import ValidatorService from "./validators/validatorService"
 import CredentialRepository from "./repositories/credentialRepository"
-import GeneratorService from "./generatorService"
-import Binder from "../binder"
 
 export default class CredentialService {
 
@@ -13,14 +11,12 @@ export default class CredentialService {
   public credentialRepository: CredentialRepository;
   public keyDerivation: KeyDerivation
   public aes: Aes
-  public generatorService: GeneratorService
 
   constructor(container: any) {
     this.validatorService = container.validatorService;
     this.credentialRepository = container.credentialRepository;
     this.keyDerivation = container.keyDerivation;
     this.aes = container.aes
-    this.generatorService = container.generatorService
   }
 
   create(user: User): Promise<any> {
@@ -33,25 +29,16 @@ export default class CredentialService {
   }
 
   update(user: User): Promise<any> {
-    
-    let credential: Credential
     return new Promise((resolve) => { resolve(this.validatorService.update(user)) })
       .then(() => this.credentialRepository.get(user.id))
-      .then((rows) => { 
-        this.validatorService.exist(rows)
-        credential = Binder.bind(rows[0], Credential.prototype)
-      })
-      .then(() => {
-        const salt = this.generatorService.saltBySource(credential.salt.toString())
-        return this.keyDerivation.encrypt(user.password, salt)
-      })
-      .then((result) => {
-        this.validatorService.password(result.data.toString(), credential.pwd.toString())
+      .then((rows) => { this.validatorService.exist(rows); return rows[0];})
+      .then((credential) => {
+
         user.pin = this.aes.decrypt(credential.pin, user.password)
         user.password = user.newPassword
         return this.encryptCredentials(user)
       })
-      .then((newCredential) => this.credentialRepository.update(newCredential))
+      .then((credential) => this.credentialRepository.update(credential))
       .then(() => { return user })
   }
 
